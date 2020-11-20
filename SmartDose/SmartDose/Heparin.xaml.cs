63d5e574f8f816c3ca7445d0a7271e0675e59eb9
Jan Řehoř <18s.rehorja@spszr.cz>
2020-11-20 22:24:32 +0100

    Design of the heparin page

---
+++ SmartDose/SmartDose/Heparin.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SmartDose
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Heparin : ContentPage
+    {
+        public Heparin()
+        {
+            InitializeComponent();
+        }
+
+        private void WeightEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void CurrentApttrEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void TargetApttrEntry_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
